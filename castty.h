3c8d51d299285a32db27e81acb1981ac5d33dcee
Devon H. O'Dell <dho@fastly.com>
2017-03-10 21:13:34 -0800

    cleanup

--- castty.h
+++ castty.h
@@ -1,12 +1,12 @@
 #ifndef CASTTY_H
 #define CASTTY_H
 
-FILE*   efopen          (const char *path, const char *mode);
-int     edup            (int oldfd);
-int     edup2           (int oldfd, int newfd);
-FILE*   efdopen         (int fd, const char *mode);
+int edup(int oldfd);
+int edup2(int oldfd, int newfd);
+FILE *efopen(const char *path, const char *mode);
+FILE *efdopen(int fd, const char *mode);
 
-void audio_init(const char *, int);
+void audio_init(const char *outfile, int flag);
 void audio_toggle(void);
 void audio_deinit(void);
 
