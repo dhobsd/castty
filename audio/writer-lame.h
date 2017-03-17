72afd89dfeb1471663fc61d67fd9b73691564741
Tomasz Kramkowski <tk@the-tk.com>
2017-03-17 19:15:52 +0000

    Make the lame audio writer optional (default enabled)
    This patch makes the modifications required to allow end users to
    disable the lame output format and compile without lame, however the
    format is enabled by default.

--- audio/writer-lame.h
+++ audio/writer-lame.h
@@ -3,7 +3,11 @@
 
 #include "writer.h"
 
+#ifdef WITH_LAME
 struct audio_writer *audio_writer_lame(FILE *outfile, int sample_rate, int nchannels,
     int buf_time_s, int mono);
+#else
+#define audio_writer_lame(...) (NULL)
+#endif
 
 #endif /* AUDIO_WRITER_LAME_H */
